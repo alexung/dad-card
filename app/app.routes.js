f381ea0947ed072951fac9f075eef774a2b79926
Alex Ung <alexung.ucsb@gmail.com>
2015-06-20 14:00:24 -0700

    Add info button

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,6 +4,10 @@ dadApp.config(['$routeProvider', function($routeProvider){
       templateUrl: "app/components/card-2015/views/dadCard2015View.html",
       controller: "dadCard2015Ctrl"
     })
+    .when('/info', {
+      templateUrl: "app/components/card-2015/views/infoView.html",
+      controller: "dadCard2015Ctrl"
+    })
     .otherwise({
       redirectTo: '/'
     });
